86101988ab2321154f33c152a5d31eed170ee3d1
Dave Swindell <cdswindell@comcast.net>
2023-02-23 22:34:30 -0500

    Work on table properties

--- src/cdspy/elements/table_property.py
+++ src/cdspy/elements/table_property.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from typing import Optional
 
 from enum import Enum, verify, UNIQUE
@@ -7,23 +8,34 @@ from .element_type import ElementType
 
 
 class _TableProperty:
+    """
+    Private class used to construct Property enums. Defines the core
+    characteristics of every Property along with the set of Table Element(s)
+    each property is applicable to
+    """
+
     def __init__(
         self,
-        _optional: bool,
+        optional: bool,
         read_only: bool,
         initializable: bool,
         tag: Optional[str],
         *args: ElementType,
     ) -> None:
-        self.optional = _optional
-        self.read_only = read_only
-        self.initializable = initializable
-        self.tag = tag
-        self.implemented_by = set(args) if args else set(ElementType)
+        self._optional = optional
+        self._read_only = read_only
+        self._initializable = initializable
+        self._tag = tag
+        self._implemented_by = set(args) if args else set(ElementType)
 
 
 @verify(UNIQUE)
 class Property(Enum):
+    """
+    CdsPy defines a number of characteristics, defined in this Property class,
+    to control, define, and express ...
+    """
+
     Label = _TableProperty(True, False, False, "lb")
     Description = _TableProperty(True, False, False, "desc")
     Tags = _TableProperty(True, False, False, "tags")
@@ -129,6 +141,98 @@ class Property(Enum):
         False, False, True, "isP", ElementType.TableContext, ElementType.Table
     )
 
+    # Table Element Properties
+    NumSubsets = _TableProperty(
+        False,
+        True,
+        False,
+        "nSets",
+        ElementType.Table,
+        ElementType.Row,
+        ElementType.Column,
+        ElementType.Group,
+    )
+    NumRows = _TableProperty(
+        False, True, False, "nRows", ElementType.Table, ElementType.Group
+    )
+    NumColumns = _TableProperty(
+        False, True, False, "nCols", ElementType.Table, ElementType.Group
+    )
+    NumCells = _TableProperty(
+        False,
+        True,
+        False,
+        "nCells",
+        ElementType.Table,
+        ElementType.Row,
+        ElementType.Column,
+        ElementType.Group,
+    )
+    NumRowsCapacity = _TableProperty(False, True, False, None, ElementType.Table)
+    NumColumnsCapacity = _TableProperty(False, True, False, None, ElementType.Table)
+    NumCellsCapacity = _TableProperty(False, True, False, None, ElementType.Column)
+    NextCellOffset = _TableProperty(False, True, False, None, ElementType.Table)
+    Derivation = _TableProperty(
+        False,
+        False,
+        False,
+        "fx",
+        ElementType.Column,
+        ElementType.Row,
+        ElementType.Cell,
+    )
+    TimeSeries = _TableProperty(
+        False, False, False, "tx", ElementType.Column, ElementType.Row
+    )
+    Affects = _TableProperty(
+        False,
+        True,
+        False,
+        None,
+        ElementType.Table,
+        ElementType.Group,
+        ElementType.Column,
+        ElementType.Row,
+        ElementType.Cell,
+    )
+    Index = _TableProperty(
+        False, True, False, None, ElementType.Row, ElementType.Column
+    )
+    IsEnforceDataType = _TableProperty(
+        False,
+        False,
+        True,
+        "isEDT",
+        ElementType.TableContext,
+        ElementType.Table,
+        ElementType.Row,
+        ElementType.Column,
+        ElementType.Cell,
+    )
+    IsInUse = _TableProperty(
+        False, True, False, None, ElementType.Row, ElementType.Column
+    )
+
+    Rows = _TableProperty(
+        False, True, False, None, ElementType.Table, ElementType.Group
+    )
+    Columns = _TableProperty(
+        False, True, False, None, ElementType.Table, ElementType.Group
+    )
+    Groups = _TableProperty(
+        False,
+        True,
+        False,
+        None,
+        ElementType.Table,
+        ElementType.Row,
+        ElementType.Column,
+        ElementType.Group,
+    )
+    Cells = _TableProperty(
+        False, True, False, None, ElementType.Row, ElementType.Column, ElementType.Group
+    )
+
     # Cell properties
     Row = _TableProperty(False, True, False, None, ElementType.Cell)
     Column = _TableProperty(False, True, False, None, ElementType.Cell)
@@ -171,21 +275,67 @@ class Property(Enum):
         ElementType.Cell,
     )
 
+    @classmethod
+    def _missing_(cls, name: object) -> Property:
+        if name is None:
+            raise ValueError(f"None is not a valid {cls.__name__}")
+
+        name = str(name).lower().strip()
+        for member in cls:
+            if member.name.lower() == name:
+                return member
+        raise ValueError(f"'{name}' is not a valid {cls.__name__}")
+
+    @classmethod
+    def by_name(cls, name: str) -> Property:
+        if name in cls.__members__:
+            return cls[name]
+        # fall back to case insensitive s
+        name = name.lower()
+        for k, v in cls.__members__.items():
+            if k.lower() == name:
+                return v
+        else:
+            raise ValueError(f"'{name}' is not a valid {cls.__name__}")
+
+    @staticmethod
+    def by_tag(tag: str) -> Optional[Property]:
+        if not tag and not tag.strip():
+            return None
+        tag = tag.strip().lower()
+        return _PROPERTIES_BY_TAG[tag] if tag in _PROPERTIES_BY_TAG else None
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Property):
+            return False
+        return self.name == other.name
+
+    def __lt__(self, other: Property) -> bool:
+        if not isinstance(other, Property):
+            raise NotImplementedError
+        return self.name < other.name
+
+    def __hash__(self) -> int:
+        return self.name.__hash__()
+
+    def is_implemented_by(self, et: ElementType) -> bool:
+        return et in self.value._implemented_by
+
     def is_read_only(self) -> bool:
-        return bool(self.value.read_only)
+        return bool(self.value._read_only)
 
     def is_mutable(self) -> bool:
-        return not self.value.read_only
+        return not self.value._read_only
 
     def is_optional(self) -> bool:
-        return bool(self.value.optional)
+        return bool(self.value._optional)
 
     def is_required(self) -> bool:
-        return not self.value.optional
+        return not self.value._optional
 
     def tag(self) -> str:
-        if self.value.tag:
-            return str(self.value.tag)
+        if self.value._tag:
+            return str(self.value._tag)
         else:
             return self.name
 
@@ -196,13 +346,29 @@ class Property(Enum):
         # handle one-offs by placing them in set
         return self in set([])
 
+    def is_numeric(self) -> bool:
+        if self.name.lower().startswith("num"):
+            return True
+
+        # handle one-offs by placing them in set
+        return self in {
+            Property.Index,
+            Property.Precision,
+            Property.CellOffset,
+            Property.NextCellOffset,
+            Property.RowCapacityIncr,
+            Property.ColumnCapacityIncr,
+        }
+
     def is_str(self) -> bool:
         # handle one-offs by placing them in set
-        return self in set(
-            [
-                Property.Label,
-                Property.Description,
-                Property.Units,
-                Property.DisplayFormat,
-            ]
-        )
+        return self in {
+            Property.Label,
+            Property.Description,
+            Property.Units,
+            Property.DisplayFormat,
+        }
+
+
+# Define static Tag map
+_PROPERTIES_BY_TAG = {p.tag().lower(): p for p in Property}
