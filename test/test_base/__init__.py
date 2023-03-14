694726bf2da77d14216a8f7051d8e49523352bd7
Dave Swindell <cdswindell@comcast.net>
2023-03-13 21:42:33 -0400

    Refactored MockObject to test_base.py

--- test/test_base/__init__.py
+++ test/test_base/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import annotations
 
+from .test_base import MockObject as MockObject
 from .test_base import TestBase as TestBase
--- test/test_base/test_base.py
+++ test/test_base/test_base.py
@@ -6,3 +6,28 @@ from typing import Any
 class TestBase:
     def init(self, *args: Any, **kwargs: Any) -> None:
         super().__init__()
+
+
+class MockObject:
+    def __init__(self, x: int):
+        self._x = x
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    def __repr__(self) -> str:
+        return f"MockObject({self.x})"
+
+    def __hash__(self) -> int:
+        return hash(self._x)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, MockObject):
+            return False
+        return self.x == other.x
+
+    def __lt__(self, other: MockObject) -> bool:
+        if not isinstance(other, MockObject):
+            raise NotImplementedError
+        return self.x < other.x
