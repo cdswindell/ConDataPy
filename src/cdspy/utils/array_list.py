9aa85442e4316e1a85a9e4377da5aa845eeb78e4
Dave Swindell <cdswindell@comcast.net>
2023-03-19 14:21:04 -0400

    ArrayList Unit Tests

--- src/cdspy/utils/array_list.py
+++ src/cdspy/utils/array_list.py
@@ -32,7 +32,7 @@ class ArrayList(MutableSequence, Generic[_T]):
                 self._list: MutableSequence[_T] = cast(MutableSequence[_T], list(source))
                 self._len = len(self._list)
             else:
-                raise NotImplementedError(f"Can not create ArrayList from {type(source)}")
+                raise NotImplementedError(f"Can not create ArrayList from {type(source).__name__}")
         elif isinstance(initial_capacity, int):
             self._list: MutableSequence[_T] = cast(MutableSequence[_T], [None] * initial_capacity)
         self._lock = RLock()
@@ -54,6 +54,15 @@ class ArrayList(MutableSequence, Generic[_T]):
             self._iter_idx += 1
             return self._list[self._iter_idx - 1]
 
+    def _get_effective_index(self, index: int, max_value: Optional[int] = None) -> int:
+        if not isinstance(index, int):
+            raise TypeError(f"'{type(index).__name__} object cannot be interpreted as an integer")
+        index = index if index >= 0 else index - self.capacity + self._len
+        if max_value is not None and int(max_value) >= 0:
+            if index > max_value:
+                index = max_value
+        return index
+
     @overload
     def __getitem__(self, index: int) -> _T:
         ...
@@ -67,11 +76,11 @@ class ArrayList(MutableSequence, Generic[_T]):
             if self._len == 0 or index > self._len or index < -self._len:
                 raise IndexError("ArrayList index out of range")
             else:
-                return self._list.__getitem__(index)
+                return self._list.__getitem__(self._get_effective_index(index))
         elif isinstance(index, slice):
             return list(self).__getitem__(index)
         else:
-            raise TypeError(f"ArrayList indices must be integers or slices, not {type(index)}")
+            raise TypeError(f"ArrayList indices must be integers or slices, not {type(index).__name__}")
 
     @overload
     def __setitem__(self, index: int, value: _T) -> None:
@@ -83,10 +92,17 @@ class ArrayList(MutableSequence, Generic[_T]):
 
     def __setitem__(self, index: int | slice, value: _T | Iterable[_T]) -> None:
         if isinstance(index, int):
-            if self._len == 0 or index > self._len or index < -self._len:
-                raise IndexError("ArrayList assignment index out of range")
+            self.ensure_capacity(abs(index) + 1)
+            if index >= 0:
+                self._list.__setitem__(index, cast(_T, value))
+                if index + 1 > self._len:
+                    self._len = index + 1
             else:
-                self._list[index] = cast(_T, value)
+                if self._len <= 0 or abs(index) > self._len:
+                    raise IndexError("ArrayList assignment index out of range")
+                self._list.__setitem__(self._get_effective_index(index), cast(_T, value))
+                if abs(index) > self._len:
+                    self._len = abs(index)
         elif isinstance(index, slice):
             capacity = self.capacity
             new_list = list(self)
@@ -95,7 +111,7 @@ class ArrayList(MutableSequence, Generic[_T]):
             self._list = new_list
             self.ensure_capacity(capacity)
         else:
-            raise TypeError(f"ArrayList indices must be integers or slices, not {type(index)}")
+            raise TypeError(f"ArrayList indices must be integers or slices, not {type(index).__name__}")
 
     @overload
     def __delitem__(self, index: int) -> None:
@@ -111,7 +127,7 @@ class ArrayList(MutableSequence, Generic[_T]):
             if self._len == 0 or index > self._len or index < -self._len:
                 raise IndexError("ArrayList assignment index out of range")
             else:
-                self._list.__delitem__(index)
+                self._list.__delitem__(self._get_effective_index(index))
                 self._len -= 1
         elif isinstance(index, slice):
             if self._list:
@@ -120,7 +136,7 @@ class ArrayList(MutableSequence, Generic[_T]):
                 self._len = len(new_list)
                 self._list = new_list
         else:
-            raise TypeError(f"ArrayList indices must be integers or slices, not {type(index)}")
+            raise TypeError(f"ArrayList indices must be integers or slices, not {type(index).__name__}")
         self.ensure_capacity(capacity)
 
     @property
@@ -143,14 +159,18 @@ class ArrayList(MutableSequence, Generic[_T]):
 
     def index(self, value: _T, start: int = 0, stop: Optional[int] = None, /) -> int:
         try:
-            return self._list.index(value, start, stop if stop else self._len)
+            return self._list.index(value, self._get_effective_index(start), stop if stop else self._len)
         except ValueError:
             raise ValueError(f"{value} is not in the ArrayList")
 
     def insert(self, idx: int, value: _T) -> None:
-        if idx < self._len:
-            self._list.insert(idx, value)
-            self._len += 1
+        if self._len:
+            index = self._get_effective_index(idx, self._len)
+        else:
+            index = 0
+        self._list.insert(index, value)
+        self._len += 1
+        self.trim_to_capacity()
 
     def extend(self, values: Iterable[_T]) -> None:
         for v in values:
@@ -163,14 +183,16 @@ class ArrayList(MutableSequence, Generic[_T]):
         if self.capacity_increment:
             self.ensure_capacity()
 
-    def pop(self, index: Optional[int] = None) -> _T:
+    def pop(self, index: int = -1) -> _T:
+        if not isinstance(index, int):
+            raise TypeError(f"'{type(index).__name__} object cannot be interpreted as an integer")
         if not self._list:
             raise IndexError("pop from empty ArrayList")
-        index = index if index is not None else self._len - 1
         if index >= self._len or index < -self._len:
             raise IndexError("pop index out of range")
+        index = self._get_effective_index(index) if index is not None else self._len - 1
         # we now know index is good; pop the backing list
-        capacity = self.capacity
+        capacity = self.capacity - 1
         val = self._list.pop(index)
         # fix up length and capacity
         self._len -= 1
@@ -216,3 +238,7 @@ class ArrayList(MutableSequence, Generic[_T]):
     def trim_to_size(self) -> None:
         if self._len < self.capacity:
             del self._list[self._len :]
+
+    def trim_to_capacity(self) -> None:
+        self.trim_to_size()
+        self.ensure_capacity()
--- test/utils/test_array_list.py
+++ test/utils/test_array_list.py
@@ -18,10 +18,12 @@ class TestArrayList(TestBase):
         assert al.capacity == 0
         assert al.capacity_increment == _DEFAULT_CAPACITY
 
-        # some operations on empty ArrayList
+        # some operations on empty ArrayList that should not raise exceptions
+        al.reverse()
         al.clear()
-        assert al is not None
-        assert al == iter(al)
+        assert al.count(0) == 0
+
+        # and some that should
         with pytest.raises(StopIteration):
             assert next(al)
         with pytest.raises(IndexError):
@@ -35,4 +37,74 @@ class TestArrayList(TestBase):
         with pytest.raises(ValueError):
             al.index(0)
 
+    def test_assignment(self) -> None:
+        al = ArrayList[int](initial_capacity=32)
+        assert al is not None
+        assert len(al) == 0
+        assert al.capacity == 32
 
+        # assign some values
+        al[3] = 14
+        al[12] = -2
+        assert len(al) == 13
+        assert al.capacity == 32
+
+        # test positive and negative indices
+        assert al[3] == 14
+        assert al[12] == -2
+
+        assert al[-1] == -2
+        assert al[-10] == 14
+
+        # test count
+        assert al.count(-2) == 1
+        assert al.count(14) == 1
+        assert al.count(2) == 0
+
+        # and index
+        assert al.index(14) == 3
+        assert al.index(-2) == 12
+        assert al.index(14, -12, -1) == 3
+        assert al.index(14, -11, -1) == 3
+        assert al.index(14, -10, -1) == 3
+        with pytest.raises(ValueError):
+            assert al.index(14, -9) == 3
+        with pytest.raises(ValueError):
+            assert al.index(14, 100) == 3
+        with pytest.raises(ValueError):
+            assert al.index(14, 0, 2) == 3
+        with pytest.raises(ValueError):
+            assert al.index(14, -6, 2) == 3
+
+        # slices
+        assert al[3:4] == [14]
+        assert al[-10:-9] == [14]
+
+        # reverse the list and retest
+        al.reverse()
+        assert len(al) == 13
+        assert al[0] == -2
+        assert al[9] == 14
+        assert al[al.index(14)] == 14
+
+        # test slice assignment
+        al[0:5] = [1, 2]
+        assert len(al) == 10
+        assert al[6] == 14
+        assert al[al.index(14)] == 14
+        assert al[1:7] == [2, None, None, None, None, 14]
+
+        # and deletion
+        del al[1:5]
+        assert len(al) == 6
+        assert al[2] == 14
+        assert al.pop(-6) == 1
+        assert len(al) == 5
+        assert al.capacity == 32
+
+        # and remove
+        al.remove(14)
+
+        # and clear()
+        al.clear()
+        assert al.capacity == _DEFAULT_CAPACITY
